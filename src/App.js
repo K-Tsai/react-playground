72cb3e737792b93d7a54f3bd3e2c425e4657eb07
Kevin Tsai <k.tsai92@gmail.com>
2019-07-22 23:13:36 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import './App.css';
+import DemonymApp from './demonymapp/demonymApp';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <DemonymApp/>
+      </div>
+    );
+  }
+}
+
+export default App
